9217278b72d0981e651261a654397619013a263b
YashMehra <yashmehra0410@gmail.com>
2024-09-29 03:18:48 +0530

    03

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -9,7 +9,7 @@ import Card from "../card/Card";
 
 const getData = async (page, cat) => {
   const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    `https://hub-blog.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,
     {
       cache: "no-store",
     }
