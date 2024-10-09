f91528b5fd508c86c0e8b7c4e7dbeefac15c6f12
YashMehra <yashmehra0410@gmail.com>
2024-10-10 02:45:57 +0530

    14

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -17,7 +17,7 @@ const Featured = () => {
           <h1 className={styles.postTitle} ><Link href="https://hub-blog.vercel.app/posts/technology-and-culture">Technology and Culture</Link></h1>
           <p className={styles.postDesc}>The effect of technology on culture is multifaceted, with both positive and negative consequences. Technology has transformed the ways in which people create, consume, and share cultural content, leading to new forms of artistic expression, communication, and interaction.
           </p>
-          <button className={styles.button}><Link href="https://hub-blog.vercel.app/posts/information-creationblog-hub">Read More</Link></button>
+          <button className={styles.button}><Link href="https://hub-blog.vercel.app/posts/technology-and-culture">Read More</Link></button>
         </div>
       </div>
     </div>
